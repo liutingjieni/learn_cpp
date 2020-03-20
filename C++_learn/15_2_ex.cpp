1609789024f3d2781cea7bac3f5a48096f5f4bb5
liutingjieni <644123183@qq.com>
2020-03-20 21:43:17 +0800

    继承

--- C++_learn/15_2_ex.cpp
+++ C++_learn/15_2_ex.cpp
@@ -6,13 +6,14 @@
  ************************************************************************/
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Quote {
 public:
     Quote() = default;
     Quote(const string &book, double sales_price) : 
-                        bookNo(book), price(sales_price) {  }
+                        bookNo(book), price(sales_price) { cout << "Quote init" << endl; }
     string isbn() const { return bookNo; }
     
     virtual double net_price(size_t n) const 
@@ -27,7 +28,8 @@ protected:
 class Bulk_quote : public Quote {
 public:
     Bulk_quote() = default;
-    Bulk_quote(const string &, double, size_t, double);
+    Bulk_quote(const string &book, double p, size_t qty, double disc) : 
+                Quote(book, p), min_qty(qty), discount(disc) { cout << "init"; };
 
     double net_price(size_t) const override;
 private:
@@ -35,10 +37,19 @@ private:
     double discount = 0.0;
 };
 
+double Bulk_quote::net_price(size_t cnt) const
+{
+    if (cnt >= min_qty) 
+        return cnt * (1 - discount) * price;
+    else 
+        return cnt * price;
+}
+
 int main()
 {
     Quote item;
-    Bulk_quote bulk;
+    Bulk_quote bulk("qq", 2, 3, 0.4);
+    cout << bulk.net_price(3);
     Quote *p = &item;
     p = &bulk;
     Quote &r = bulk;
