3727eee0c79d8b238ae32dad7144990c975c7f51
liutingjieni <644123183@qq.com>
2020-03-24 22:55:03 +0800

    add

--- C++_learn/15_2_ex.cpp
+++ C++_learn/15_2_ex.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
+#include <set>
 using namespace std;
 
 class Quote {
@@ -44,6 +46,30 @@ double Bulk_quote::net_price(size_t cnt) const
     else 
         return cnt * price;
 }
+double print_total(ostream &os, Quote &s, size_t num)
+    { os  << s.net_price(num) << endl; }
+
+class Basket {
+public:
+    void add_item(const shared_ptr<Quote> &sale) 
+                { items.insert(sale); }
+    double total_receipt(ostream &) const;
+private:
+    static bool compare(const shared_ptr<Quote> &lhs, const shared_ptr<Quote> &rhs)
+                { return lhs->isbn() < rhs->isbn(); }
+    multiset<shared_ptr<Quote>, decltype(compare) *> items{compare};
+};
+
+double Basket::total_receipt(ostream &os) const
+{
+    double sum = 0.0;
+    for (auto iter = items.cbegin(); iter != items.cend(); 
+              iter = items.upper_bound(*iter)) {
+        sum += print_total(os, **iter, items.count(*iter));
+    }
+    os << "Total Sale:" << sum << endl;
+    return sum;
+};
 
 int main()
 {
@@ -53,4 +79,6 @@ int main()
     Quote *p = &item;
     p = &bulk;
     Quote &r = bulk;
+
+    Basket bsk;
 }
