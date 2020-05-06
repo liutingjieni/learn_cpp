6a6c3bff4bc0a9d9da89fc2c992e5fd2d4dc634b
liutingjieni <644123183@qq.com>
2020-05-06 14:38:52 +0800

    assert

---
+++ muduo/assert.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: assert.cpp
+	> Author:jieni 
+	> Mail: 
+	> Created Time: 2020年05月06日 星期三 14时26分40秒
+ ************************************************************************/
+#define NDEBUG
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+int main()
+{
+    int t = 0;
+    assert(t);
+}
