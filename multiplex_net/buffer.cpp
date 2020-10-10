e54c6adeac83016ded728c5da741c4a3412eab92
liutingjieni <644123183@qq.com>
2020-10-10 22:50:36 +0800

    append & retreive

--- multiplex_net/buffer.cpp
+++ multiplex_net/buffer.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <vector>
 #include <assert.h>
+#include <sys/uio.h>
 using namespace std;
 
 class buffer {
@@ -82,11 +83,11 @@ public:
         write_index_ += len;
     }
 
-    //收到数据包追加在buffer后面
+    //收到数据包追加在buffer后面, 判断writeable的空间是否大于要写入的数据空间, 如果是直接写入, 否则移动readable或者重新分配空间
     void append(const char *data, size_t len)
     {
         ensure_writable_bytes(len);
-        copy(data, data + len, begin_write());
+        copy(data, data + len, begin_write()); //复制要写入的数据到buffer中
         has_written(len);
     }
 
@@ -95,12 +96,14 @@ public:
         append(static_cast<const char *>(data), len);
     }
 
+    // writable不够的时候
     void make_space(size_t len)
     {
+        //buffer重新分配空间, 是使用之前vector多于的空间还是重新分配都有vector容器操作
         if (writable_bytes() + prependable_bytes() < len + k_cheap_prepend) {
             buffer_.resize(write_index_ + len);
         }
-        else {
+        else { //prepend + writable > 要写入的数据长度, 移动readable 
             assert(k_cheap_prepend < read_index_);
             size_t readable = readable_bytes();
             copy(begin() + read_index_, begin() + write_index_, begin() + k_cheap_prepend);
@@ -109,9 +112,70 @@ public:
             assert(readable == readable_bytes());
         }
     }
-    
+   
+    // 返回readable的首地址
+    const char *peek() const
+    {
+        return begin() + read_index_;
+    }
 
+    //要读取所有的readable的数据
+    void retrieve_all()
+    {
+        read_index_ = k_cheap_prepend;
+        write_index_ = k_cheap_prepend;
+    }
 
+    // 判断len的大小, 处理read_index_
+    void retrieve(size_t len)
+    {
+        assert(len <= readable_bytes());
+        if (len < readable_bytes()) {
+            read_index_ += len;
+        } else {
+            retrieve_all();
+        }
+    }
+
+    //将要读取的数据放入到result当中去
+    string retrieve_as_string(size_t len)
+    {
+        assert(len <= readable_bytes());
+        string result(peek(), len);
+        retrieve(len);
+        return result;
+    }
+
+    string retrieve_all_as_string()
+    {
+        return retrieve_as_string(readable_bytes());
+    }
+
+    //利用临时栈上空间,避免了每个连接的初始Buffer过大造成的内存浪费, 
+    //也避免反复调用read()的系统调用(由于缓冲区足够大, 通常一次readv()系统调用就能够读完所有数据)
+    size_t read_fd(int fd, int *saved_errno)
+    {
+        char extrabuf[65536];
+        struct iovec vec[2];
+        const size_t writable = writable_bytes();
+        vec[0].iov_base= begin() + write_index_;
+        vec[0].iov_len = writable;
+        vec[1].iov_base = extrabuf;
+        vec[1].iov_len = sizeof(extrabuf);
+
+        const int iovcnt = (writable < sizeof extrabuf) ? 2 : 1;
+        const size_t n = readv(fd, vec, iovcnt);
+
+        if (n < 0) {
+            *saved_errno = errno;
+        } else if (static_cast<size_t>(n) <= writable) {
+            write_index_ += n;
+        } else {
+            write_index_ = buffer_.size();
+            append(extrabuf, n - writable);
+        }
+        return n;
+    }
 private:
     vector<char> buffer_;
     size_t read_index_;
