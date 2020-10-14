2f36512e411b69b4397b0e816af7cf00addf92c8
liutingjieni <644123183@qq.com>
2020-10-14 18:44:07 +0800

    设计模式的必要性 二进制的复用性 抽象

--- Design_Patterns/shape.cpp
+++ Design_Patterns/shape.cpp
@@ -5,8 +5,7 @@
 	> Created Time: 2020年10月13日 星期二 18时50分18秒
  ************************************************************************/
 
-#include <iostream>
-using namespace std;
+#include <vector>
 
 class Point {
     public:
@@ -14,26 +13,99 @@ class Point {
         int y;
 
 };
-线
 class Line {
 public:
+    Point start;
+    Point end;
+
+    Line(const Point& start, const Point& end)
+    {
+        this->start = start;
+        this->end = end;
+    }
+};
   
-  }
-  
-  //矩形
-  class Rect {
-  }
-  
-  class MainFrom : public Form {
-  private:
-      privatepoint p1, p2;
-      p2vector<Line> lineVector;
-      lineVectorvector<Rect> rectVector;
-      pritected:
-          pritectedvirtual void onMouseDown(const MouseEventArgs& e);
-          MouseEventArgsvirtual void onMouseUp(const MouseEventArgs& e);
-          MouseEventArgsvirtual void onPoint(const MouseEventArgs& e)
-          }
- 
- 
+//矩形
+class Rect {
+public:
+    Point leftUp;
+    int width;
+    int height;
+
+    Rect(const Point& leftUp, int width, int height)
+    {
+        this->leftUp = leftUp;
+        this->width = width;
+        this->height = height;
+    }
+};
+
+class From;
+class MainFrom : public Form {
+private:
+    Point p1, p2;
+    vector<Line> lineVector;
+    vector<Rect> rectVector;
+    
+    MainFrom()
+    {
+        //...
+    }
+protected:
+    virtual void onMouseDown(const MouseEventArgs& e);
+    virtual void onMouseUp(const MouseEventArgs& e);
+    virtual void onPoint(const MouseEventArgs& e);
+};
+
+void MainFrom::onMouseDown(const MouseEventArgs& e)
+{
+    p1.x = e.x;
+    p1.y = e.y;
+
+    From::onMouseDown(e);
 }
+
+void MainFrom::onMouseUp(const MouseEventArgs& e)
+{
+    p2.x = e.x;
+    p2.y = e.y;
+
+    if (rdoLine.checked) {
+        Line line(p1, p2);
+        lineVector.push_back(line);
+    }
+    else if (rdoRect.checked) {
+        int width = abs(p2.x - p1.x);
+        int height = abs(p2.y - p1.y);
+        Rect rect(p1, width, height);
+        rectVector.push_back(rect);
+    }
+    else if () {
+        //...
+        CircleVector.push_back(circle);
+    }
+
+    //...
+    this->refresh();
+    From::onMouseUp(e);
+}
+
+void MainFrom::onPoint(const PointEventArgs& e)
+{
+    for (int i = 0; i < lineVectror.size(); i++) {
+        e.Graphics.DrawLine(Pens.red, lineVector[i].start.x, lineVector[i].start.y,
+                                      lineVector[i].end.x, lineVector[i].end.y);
+    }
+    for (int i = 0; i < rectVectror.size(); i++) {
+        e.Graphics.DrawRectangle(Pens.red, RectVector[i].leftUp, 
+                                           RectVector[i].width, 
+                                           RectVector[i].height);
+    }
+    //改变
+    for (int i = 0; i < circleVectror.size(); i++) {
+        e.Graphics.DrawCircle(Pens.red, circleVector[i]);
+    }
+
+    From::onPoint(e);
+}
+
--- Design_Patterns/shape_2.cpp
+++ Design_Patterns/shape_2.cpp
@@ -43,13 +43,28 @@ public:
     int width;
     int height;
 
-    Rect(const Point& leftUp, int width, int height) {  }
+    Rect(const Point& leftUp, int width, int height) 
+    {
+       this->leftUp = leftUp;
+	   this->width = width;
+       this->height = height;
+    }
     virtual void Draw (const Graphics& g) 
     { 
         g.DrawReactangle(Pens.red, leftUp, width, height); 
     }
+};
 
-}
+//增加
+class Circle : public Shape{
+public:
+	//实现自己的Draw，负责画自己
+	virtual void Draw(const Graphics& g){
+		g.DrawCircle(Pens.Red,
+			...);
+	}
+
+};
 
 class MainFrom : public From {
 private:
@@ -71,5 +86,43 @@ void MainFrom:: OnMouseUP(const MouseEventArgs& e)
 {
     p2.x = e.x;
     p2.y = e.y;
-
+    //...
+	Form::OnMouseDown(e);
 }
+
+void MainForm::OnMouseUp(const MouseEventArgs& e){
+	p2.x = e.X;
+	p2.y = e.Y;
+
+	if (rdoLine.Checked){
+		shapeVector.push_back(new Line(p1,p2));
+	}
+	else if (rdoRect.Checked){
+		int width = abs(p2.x - p1.x);
+		int height = abs(p2.y - p1.y);
+		shapeVector.push_back(new Rect(p1, width, height));
+	}
+	//改变
+	else if (...){
+		//...
+		shapeVector.push_back(circle);
+	}
+
+	//...
+	this->Refresh();
+
+	Form::OnMouseUp(e);
+}
+
+void MainForm::OnPaint(const PaintEventArgs& e){
+
+	//针对所有形状
+	for (int i = 0; i < shapeVector.size(); i++){
+
+		shapeVector[i]->Draw(e.Graphics); //多态调用，各负其责
+	}
+
+	//...
+	Form::OnPaint(e);
+}
+
